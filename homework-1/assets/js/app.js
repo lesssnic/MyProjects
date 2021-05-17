1081cfdacfeb1c4c533df99df4860b351a752235
lesssnic <antoxxxa.ua@gmail.com>
2021-05-17 19:30:28 +0300

    onclick-work

--- homework-1/assets/js/app.js
+++ homework-1/assets/js/app.js
@@ -1,16 +1,21 @@
 
 
 let rate        = [10, 12, 22, 24, 32, 35, 37];
-let state      = [0, 9951, 40526, 86376, 164926, 209426, 523600];
+let single      = [0, 9951, 40526, 86376, 164926, 209426, 523600];
 let household   = [0, 14201, 54201, 86351, 164901, 209401, 523600];
 let widow       = [0, 19901, 81051, 172751, 329851, 418851, 628300];
 let separately  = [0, 9951, 40526, 86376, 164926, 209426, 314151];
 
-let tax = 0;
 
-function taxrate(income){
-    
+
+
+function taxrate(){
+    let tax = 0;
+    let income = +incomeSum.value;
+    let state = single;
+  //  console.log(state);
     let incomeT = income;
+
     for (i = 0; i < rate.length; i++ ) {
         if ((income - state[i+1]) > 0) {
 
@@ -20,11 +25,13 @@ function taxrate(income){
         }else {
             
             tax = Math.round((tax + (incomeT * rate[i])/100)*100)/100;
+            console.log(tax);
             break;
         }
     }
-    
-    return tax;
+    console.log(tax);
+
+    return taxSum.innerHTML = tax + '  USD';
 }
-console.log( 'Налоги:', taxrate(1) ,'USD');
+//console.log( 'Налоги:', taxrate(679886754) ,'USD');
 
